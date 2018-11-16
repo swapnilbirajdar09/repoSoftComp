f61bfa6d945d00fce26c356a1b65627f1adc08d8
vaidehi <vaidehi.bizmotech@gmail.com>
2018-11-16 18:15:07 +0530

    commit contact us

---
+++ application/controllers/Contact_us.php
@@ -0,0 +1,105 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+class Contact_us extends CI_Controller {
+
+    public function __construct() {
+        parent::__construct();
+        // load common model
+     $this->load->model('admin/Setting_model');
+    }
+
+    // main index function
+    public function index() {
+        $data['contact_email'] = $this->Setting_model->getAllcompany_details();
+        $this->load->view('includes/user/header');
+        $this->load->view('pages/user/Contact_us',$data); 
+        $this->load->view('includes/user/footer');
+    }
+
+     
+
+    public function sendContactEmail() {
+        extract($_POST);
+        $admin_email = $this->adminprofile_model->getAdminEmail();
+        $adminEmail = $admin_email[0]['admin_email'];
+        $config = Array(
+            'protocol' => 'smtp',
+            'smtp_host' => 'mx1.hostinger.in',
+            'smtp_port' => '587',
+            'smtp_user' => 'support@jumlakuwait.com', // change it to yours
+            'smtp_pass' => 'Descartes@1990', // change it to yours
+            'mailtype' => 'html',
+            'charset' => 'utf-8',
+            'wordwrap' => TRUE
+        );
+        $config['smtp_crypto'] = 'tls';
+
+        $this->load->library('email', $config);
+        $this->email->set_newline("\r\n");
+        $this->email->from('support@jumlakuwait.com', "Admin Team");
+        $this->email->to($adminEmail, 'Admin Team');
+        $this->email->subject("Parinay-Contact Form");
+        $this->email->message("<html>"
+                . "<head>"
+                . "</head>"
+                . "<body>"
+                . "<p><label><h3><b>Contact Form</label></b></h3></p>"
+                . "<p><label>Contact form has been submitted by: Name:- $name </label></p>"
+                . "<p><label>Email Id:- $email </label></p>"
+                . "<p><label>Subject:- $number</label></p>"
+                . "<p><label>For The Purpose Of: $message </label></p>"
+                . "</body>"
+                . "</html>");
+        if (!$this->email->send()) {
+            echo '<div class="alert alert-danger" style="margin-bottom:5px">
+            <strong>Message Sending Failed.</strong> 
+            </div>
+            <script>
+            window.setTimeout(function() {
+              $(".alert").fadeTo(1000, 0).slideUp(1000, function(){
+                 $(this).remove(); 
+                 location.reload();
+             });
+            }, 100);
+            </script>';
+        } else {
+
+            $this->load->library('email', $config);
+            $this->email->set_newline("\r\n");
+            $this->email->from('support@jumlakuwait.com', "Admin Team");
+            $this->email->to($email, $name);
+            $this->email->subject("Parinay Customer Support");
+            $this->email->message('<html>
+            <head>
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <link rel="stylesheet" href="http://jobmandi.in/css/bootstrap/bootstrap.min.css">
+            <script src="http://jobmandi.in/css/bootstrap/jquery.min.js"></script>
+            <script src="http://jobmandi.in/css/bootstrap/bootstrap.min.js"></script>
+            </head>
+            <body>
+            <div class="container col-lg-8" style="box-shadow: 0 2px 4px 0 rgba(0,0,0,0.16),0 2px 10px 0 rgba(0,0,0,0.12)!important;margin:10px; font-family:Candara;">
+            <h2 style="color:#4CAF50; font-size:30px">ParinayCustomer Support</h2>
+            <h3 style="font-size:15px;"> Your message was successfully sent!.<br><br>Thank you for contacting us, we will reply to your inquiry as soon as possible!</h3>
+            <div class="col-lg-12">
+            <div class="col-lg-4"></div>
+            <div class="col-lg-4"></div>
+            </div>
+            <h4 style="font-size:15px;"><b>Thank You..!</b></h4>
+            </div>
+            </body></html>');
+
+            if ($this->email->send()) {
+                echo '<div class="alert alert-success alert-dismissible" role="alert" style="margin-bottom:5px">
+                     <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+            <strong>Message Sent Successfully..!</strong> 
+            </div>';
+            } else {
+                echo '<div class="alert alert-danger alert-dismissible" style="margin-bottom:5px">
+                      <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+            <strong>Message Sending Failed..!</strong> 
+            </div>';
+            }
+        }
+    }
+}
---
+++ application/views/pages/user/contact_us.php
@@ -0,0 +1,144 @@
+<section class="wow fadeIn bg-light-gray inset-35px-tb page-title-small top-space">
+        <div class="container">
+          <div class="row equalize xs-equalize-auto">
+            <div class="col-lg-8 col-md-6 col-sm-6 col-xs-12 display-table">
+              <div class="display-table-cell vertical-align-middle text-left xs-text-center">
+                <h1 class="text-font-sec text-extra-dark-gray text-medium no-offset-bottom text-uppercase">Contact us</h1>
+              </div>
+            </div>
+            <div class="col-lg-4 col-md-6 col-sm-6 col-xs-12 display-table text-right xs-offset-15px-top">
+              
+            </div>
+          </div>
+        </div>
+      </section>
+      <section class="wow fadeIn big-section">
+        <div class="container">
+          <div class="row">
+            <div class="col-md-8 col-sm-12 col-xs-12 text-center center-col"><span class="text-font-sec text-small text-uppercase">Let's cooperate</span>
+              <h2 class="text-font-sec text-semi-bold letter-spacing-minus-1 text-extra-dark-gray">We are ready to help you</h2>
+              <p class="width-75 center-col xs-width-100">Cum zirbus ire, omnes nixes locus fidelis, camerarius liberies. Fidelis, secundus gloss virtualiter transferre de superbus, talis lapsus. Agripeta assimilants, tanquam regius diatria. Cur liberi studere? Varius idoleums ducunt ad orgia. Sunt absolutioes resuscitabo placidus, dexter genetrixes.</p><a class="btn btn-large btn-transparent-dark-gray offset-10px-top inner-link" href="#start-your-project">Get started</a>
+            </div>
+          </div>
+        </div>
+      </section>
+      <!-- Contact info-->
+      <section class="no-inset bg-extra-dark-gray">
+        <div class="container-fluid">
+          <div class="row equalize sm-equalize-auto">
+            <div class="col-md-6 col-sm-12 no-inset cover-background sm-height-450px xs-height-350px wow fadeInLeft" style="background: url('<?php echo base_url(); ?>assets/user/images/contact-us-classic-1-1000x700.jpg')"></div>
+            <div class="col-md-6 col-sm-12 no-inset col-2-nth wow fadeInRight">
+              <div class="col-md-6 col-sm-6 col-xs-12 display-table bg-extra-dark-gray height-350px last-paragraph-no-margin">
+                <div class="display-table-cell vertical-align-middle text-center"><i class="linearicons-map2 text-secondary icon-medium offset-25px-bottom"></i>
+                  <div class="text-white text-uppercase text-font-sec text-medium offset-5px-bottom">Our address</div>
+                  <p class="width-60 md-width-80 center-col text-medium">222 Stonybrook St. Los Angeles, CA 90044</p>
+                </div>
+              </div>
+              <div class="col-md-6 col-sm-6 col-xs-12 display-table bg-black height-350px last-paragraph-no-margin">
+                <div class="display-table-cell vertical-align-middle text-center"><i class="linearicons-laptop-phone text-secondary icon-medium offset-25px-bottom"></i>
+                  <div class="text-white text-uppercase text-font-sec text-medium offset-5px-bottom">Phones</div>
+                  <p class="center-col text-medium no-margin">Phone: 1-818-539-9985</p>
+                  <p class="center-col text-medium">Fax: 1-818-524-9156</p>
+                </div>
+              </div>
+              <div class="col-md-6 col-sm-6 col-xs-12 display-table bg-black height-350px last-paragraph-no-margin">
+                <div class="display-table-cell vertical-align-middle text-center"><i class="linearicons-voicemail text-secondary icon-medium offset-25px-bottom"></i>
+                  <div class="text-white text-uppercase text-font-sec text-medium offset-5px-bottom">Emails</div>
+                  <p class="center-col text-medium no-margin"><a href="mailto:#">info@demolink.org</a></p>
+                  <p class="center-col text-medium"><a href="mailto:#">mail@demolink.org</a></p>
+                </div>
+              </div>
+              <div class="col-md-6 col-sm-6 col-xs-12 display-table bg-extra-dark-gray height-350px last-paragraph-no-margin">
+                <div class="display-table-cell vertical-align-middle text-center"><i class="linearicons-clock3 text-secondary icon-medium offset-25px-bottom"></i>
+                  <div class="text-white text-uppercase text-font-sec text-medium offset-5px-bottom">Opening hours</div>
+                  <p class="center-col text-medium no-margin">Mon to Sat - 9 AM to 11 PM</p>
+                  <p class="center-col text-medium">Sunday - 10 AM to 6 PM</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section class="wow fadeIn" id="start-your-project">
+        <div class="container">
+          <div class="row">
+            <div class="col-lg-6 col-md-6 col-sm-8 col-xs-12 center-col offset-eight-bottom sm-offset-40px-bottom xs-offset-30px-bottom text-center last-paragraph-no-margin">
+              <h5 class="text-font-sec text-semi-bold text-extra-dark-gray text-uppercase">Get in touch with us</h5>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+            </div>
+          </div>
+          <form id="contact_us_form" class="rd-mailform text-left" data-form-output="form-output-global" data-form-type="contact" method="post" >
+            <div class="row row-20">
+              <div class="col-md-6">
+                <div class="form-wrap">
+                  <label class="form-label" for="contact-name">Name</label>
+                  <input class="form-input" id="contact-name" type="text" name="name" data-constraints="@Required">
+                </div>
+              </div>
+              <div class="col-md-6">
+                <div class="form-wrap">
+                  <label class="form-label" for="contact-phone">Phone</label>
+                  <input class="form-input" id="contact-phone" type="text" name="phone" data-constraints="@Required @Numeric">
+                </div>
+              </div>
+              <div class="col-md-6">
+                <div class="form-wrap">
+                  <label class="form-label" for="contact-email">E-Mail</label>
+                  <input class="form-input" id="contact-email" type="email" name="email" data-constraints="@Required @Email">
+                </div>
+              </div>
+              <div class="col-md-6">
+                <div class="form-wrap">
+                  <div class="form-wrap">
+                  <label class="form-label" for="contact-subject">Subject</label>
+                  <input class="form-input" id="contact-subject" type="text" name="email" data-constraints="@Required">
+                </div>
+                </div>
+              </div>
+              <div class="col-md-12">
+                <div class="form-wrap">
+                  <label class="form-label" for="contact-message">Message</label>
+                  <textarea class="form-input" id="contact-message" name="message" data-constraints="@Required"></textarea>
+                </div>
+              </div>
+            </div>
+            <div class="form-button group-sm text-center text-lg-left">
+              <button class="btn btn-secondary btn-rounded btn-large offset-20px-top" type="submit">send message</button>
+            </div>
+          </form>
+        </div>
+      </section>
+      <section class="wow fadeIn bg-light-gray">
+     
+         
+            <div class="col-md-12 text-center social-style-4 border round"><span class="text-medium text-medium text-uppercase display-block text-font-sec text-extra-dark-gray offset-30px-bottom">Follow Us</span>
+              <div class="social-icon-style-4">
+               <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3783.525124457551!2d73.83154621436857!3d18.505157274476428!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bc2bf8d90261449%3A0xbbb511df2afe0c9c!2sBizmo+Technologies!5e0!3m2!1sen!2sin!4v1542367197142" width="100%" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+              </div>
+            
+        </div>
+      </section>
+
+      <script>
+    $(function () {
+        $("#contact_us_form").submit(function () {
+            dataString = $("#contact_us_form").serialize();
+            $("#submitbtn").html('<span class="w3-card w3-padding-small w3-margin-bottom w3-round"><i class="fa fa-spinner fa-spin w3-large"></i> <b>Sending Message. Hang on...</b></span>');
+            $.ajax({
+                type: "POST",
+                url: BASE_URL + "user/contact_us/sendContactEmail",
+                //dataType : 'text',
+                data: dataString,
+                return: false, //stop the actual form post !important!
+                success: function (data) {
+                    console.log(data);                    
+                    $("#submitbtn").html('<span>Send Message</span>');
+                    $("#fpasswd_err").html(data);
+                    $('form :input').val('');
+                }
+            });
+            return false;  //stop the actual form post !important!
+
+        });
+    });
+</script>
